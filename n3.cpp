171000ba7dfedc2743bb3869f3275a4d3ab4577e
JosselineYolette <127876852+JosselineYolette@users.noreply.github.com>
2023-09-05 23:25:52 -0500

    .
    .

---
+++ n3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    cout<<"suma de 2 numeros \n ingresa el numero 1 \n";
+    
+    int num = 0  ;
+    int num2 = 0 ;
+    
+    cout<<"ingresa el numero 1\n";
+    cin>>num;
+    cout<<"ingresa el numero 2\n";
+    cin>>num2;
+    int suma = num + num2;
+    cout<<"la suma de los 2 numeros es : "<< suma;
+    return 0;
+}
---
+++ n9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float nt1,nt2,nt3, result ;
+    cout<<"ingresa tus notas para saber la definitiva de la materia\nNota 1:\n ";
+    cin>>nt1;
+    cout<<"Nota 2:\n";
+    cin>>nt2;
+    cout<<"nota 3:\n";
+    cin>>nt3;
+    
+}
